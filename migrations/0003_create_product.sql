052a5ddeab15dd92b2113b6fdefb982a29a612a2
Sofija1999 <sofija.dokmanovic99@gmail.com>
2023-06-28 15:26:50 +0200

    update migrations create product

--- migrations/0003_create_product.sql
+++ migrations/0003_create_product.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS egw.product (
-	id uuid NOT NULL,
+	id uuid NOT NULL DEFAULT uuid_generate_v4(),
 	"name" varchar NOT NULL,
 	short_description varchar NOT NULL,
 	description varchar NOT NULL,
-	price int4 NOT NULL,
-	created_at time NULL,
-	updated_at time NULL,
+	price numeric NULL DEFAULT 0,
+	created_at timestamp NULL DEFAULT now(),
+	updated_at timestamp NULL DEFAULT now(),
 	CONSTRAINT product_pk PRIMARY KEY (id)
 );
